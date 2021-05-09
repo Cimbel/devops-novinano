0a40fe7379493491041d2fd111bd853879167f31
Cimbel <55352072+Cimbel@users.noreply.github.com>
2021-05-10 01:59:15 +0300

    added route tables and associations with route tables

--- iaac_terraform_kubernetes/vpc.tf
+++ iaac_terraform_kubernetes/vpc.tf
@@ -13,6 +13,30 @@ resource "aws_internet_gateway" "novinano-igw" {
 }
 
 
+resource "aws_route_table" "novinano-public-subnets-route-table" {
+  vpc_id = aws_vpc.novinano-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.novinano-igw.id
+  }
+
+  tags = { Name = "novinano-public-subnets-route-table" }
+}
+
+
+resource "aws_route_table_association" "association_public_subnet_a1" {
+  subnet_id      = aws_subnet.novinano-public-subnet-a1.id
+  route_table_id = aws_route_table.novinano-public-subnets-route-table.id
+}
+
+
+resource "aws_route_table_association" "association_public_subnet_b1" {
+  subnet_id      = aws_subnet.novinano-public-subnet-b1.id
+  route_table_id = aws_route_table.novinano-public-subnets-route-table.id
+}
+
+
 resource "aws_subnet" "novinano-public-subnet-a1" {
   cidr_block              = "10.0.11.0/24"
   vpc_id                  = aws_vpc.novinano-vpc.id
