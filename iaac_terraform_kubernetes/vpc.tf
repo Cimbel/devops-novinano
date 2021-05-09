7492e33f3bd6549803f05fc6257f10e587a927a0
Cimbel <55352072+Cimbel@users.noreply.github.com>
2021-05-10 00:48:10 +0300

    added two public subnets

--- iaac_terraform_kubernetes/vpc.tf
+++ iaac_terraform_kubernetes/vpc.tf
@@ -4,3 +4,23 @@ resource "aws_vpc" "novinano-vpc" {
 
   tags = { Name = "novinano-vpc" }
 }
+
+
+resource "aws_subnet" "novinano-public-subnet-a1" {
+  cidr_block              = "10.0.11.0/24"
+  vpc_id                  = aws_vpc.novinano-vpc.id
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = { Name = "novinano-public-subnet-a1" }
+}
+
+
+resource "aws_subnet" "novinano-public-subnet-b1" {
+  cidr_block              = "10.0.21.0/24"
+  vpc_id                  = aws_vpc.novinano-vpc.id
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[1]
+
+  tags = { Name = "novinano-public-subnet-b1" }
+}
