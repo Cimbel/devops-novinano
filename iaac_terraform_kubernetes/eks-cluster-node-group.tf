fc68095c2d4f7db9f79acc1bd6b40036b8ea745a
Cimbel <55352072+Cimbel@users.noreply.github.com>
2021-05-10 20:08:06 +0300

    changed max size, and renamed name of eks-cluster-node-group

--- iaac_terraform_kubernetes/eks-cluster-node-group.tf
+++ iaac_terraform_kubernetes/eks-cluster-node-group.tf
@@ -1,4 +1,4 @@
-resource "aws_eks_node_group" "example" {
+resource "aws_eks_node_group" "novinano-eks-cluster-node-group" {
   cluster_name    = aws_eks_cluster.novinano-eks-cluster.name
   node_group_name = "novinano-eks-cluster-node-group"
   node_role_arn   = aws_iam_role.novinano-eks-cluster-node-group-role.arn
@@ -10,7 +10,7 @@ resource "aws_eks_node_group" "example" {
 
   scaling_config {
     desired_size = 1
-    max_size     = 2
+    max_size     = 1
     min_size     = 1
   }
 
