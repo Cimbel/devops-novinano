7e797ecaea2a2dae14de40c502a44eda46e87643
Cimbel <55352072+Cimbel@users.noreply.github.com>
2021-05-11 01:34:33 +0300

    added new options into vpc

--- iaac_terraform_kubernetes/vpc.tf
+++ iaac_terraform_kubernetes/vpc.tf
@@ -4,8 +4,10 @@ locals {
 
 
 resource "aws_vpc" "novinano-vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = { Name = "novinano-vpc" }
 }
